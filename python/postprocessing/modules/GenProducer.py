b44392a2710a11df04576ac08448a07426ca5a71
Ben Barton <99bbarton@gmail.com>
2023-12-18 13:25:33 +0100

    Add DR calculations to GenProducer

--- python/postprocessing/modules/GenProducer.py
+++ python/postprocessing/modules/GenProducer.py
@@ -26,6 +26,17 @@ def getProdChain(idx, genParts):
 
 # -----------------------------------------------------------------------------------------------------------------------------
 
+def getDecayChain(idx, genParts):
+    chain = []
+    for dauIdx in range(idx + 1, genParts._len):
+        dauPart = genParts[dauIdx]
+        if dauPart.genPartIdxMother == idx:
+            chain.append(dauIdx)
+    return chain
+
+
+# -----------------------------------------------------------------------------------------------------------------------------
+
 # Check whether the production chain contains a particle matching idx and/xor pdgID
 # prodChain : The production chain list as returned by getProdChain()
 # idx : the idx to GenPart to try to match. If not specified, will not try to match indices
@@ -71,7 +82,15 @@ class GenProducer(Module):
         self.out.branch("Gen_tsTauIdx", "I") #Idx to the last copy of the tau decay product of the taustar in GenPart
         self.out.branch("Gen_tauIdx", "I") #Idx to the last copy of the tau produced alongside the taustar in GenPart
         self.out.branch("Gen_zIdx", "I") #Idx to the last copy of the Z from the taustar decay in GenPart
+        self.out.branch("Gen_zDau1Idx", "I") #Idx to the first daughter of the Z at zIdx in GenPart
+        self.out.branch("Gen_zDau2Idx", "I") #Idx to the second daughter of the Z at zIdx in GenPart
+        self.out.branch("Gen_zDM","I") #0 = hadronic, 1=electrons, 2=muons, 3=taus
+        self.out.branch("Gen_dr_tsTauTau", "F") #DeltaR between tsTau and Tau
+        self.out.branch("Gen_dr_tsTauZ", "F") # DeltaR between tsTau and Z
+        self.out.branch("Gen_dr_tauZ", "F") #DeltaR between tau and Z
+        self.out.branch("Gen_dr_zDaus", "F") #DeltaR between the two Z daughters
 
+        #self.out.branch("Gen_ch","I") #1 = ETau, 2=MuTau, 3=TauTau
 
     def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
         pass
@@ -84,9 +103,17 @@ class GenProducer(Module):
         tsTauIdx = -1
         tauIdx = -1
         zIdx = -1
+        zDau1Idx = -1
+        zDau2Idx = -1
+        zDM = -1
+        dr_tsTauTau = -999
+        dr_tsTauZ = -999
+        dr_tauZ = -999
+        dr_zDaus = -999
 
         genParts = Collection(event, "GenPart")
 
+        foundAll = False
         for idx, genPart in enumerate(genParts):
             #We only care about the last copy of relevant particles
             if not genPart.statusflag('isLastCopy'):
@@ -104,11 +131,52 @@ class GenProducer(Module):
                 prodChain = getProdChain(idx, genParts)
                 if prodChainContains(prodChain, pdgID = 4000015): #If this Z is the taustar decay product
                     zIdx = idx
-                    
+                    decayChain = getDecayChain(zIdx, genParts)
+                    if len(decayChain) != 2:
+                        print("WARNING: Number of Z decay chain particles is != 2. Will write -1 indices for this event")
+                    else:
+                        zDau1Idx = decayChain[0]
+                        zDau2Idx = decayChain[1]
+                        if abs(event.GenPart_pdgid[zDau1Idx]) <= 6:
+                            zDM = 0
+                        elif abs(event.GenPart_pdgid[zDau1Idx]) == 11:
+                            zDM = 1
+                        elif abs(event.GenPart_pdgid[zDau1Idx]) == 13:
+                            zDM = 2
+                        elif abs(event.GenPart_pdgid[zDau1Idx]) == 15:
+                            zDM = 3
+            foundAll = (tsTauIdx >= 0) and (tauIdx >= 0) and (zIdx >= 0) and (zDau1Idx >=0) and (zDau2Idx >= 0)
+            if foundAll:
+                break
+        #End idx finding
+
+        #Calculate DeltaR between the interesting particles
+        if foundAll:
+            tsTau = genParts[tsTauIdx]
+            tau = genParts[tauIdx]
+            z = genParts[zIdx]
+            zDau1 = genParts[zDau1Idx]
+            zDau2 = genParts[zDau2Idx]
+
+            dr_tsTauTau = tsTau.DeltaR(tau)
+            dr_tsTauZ = tsTau.DeltaR(z)
+            dr_tauZ = tau.DeltaR(z)
+            dr_zDaus = zDau1.DeltaR(zDau2)
+        else:
+            print("WARNING: In GenProducer: All interesting gen particles were not found. Will not calculate DeltaR's")
+
+        #Write output to tree
         self.out.fillBranch("Gen_tsIdx", tsIdx)
         self.out.fillBranch("Gen_tsTauIdx", tsTauIdx)
         self.out.fillBranch("Gen_tauIdx", tauIdx)
         self.out.fillBranch("Gen_zIdx", zIdx)
+        self.out.fillBranch("Gen_zDau1Idx",zDau1Idx)
+        self.out.fillBranch("Gen_zDau2Idx",zDau2Idx)
+        self.out.fillBranch("Gen_zDM", zDM)
+        self.out.fillBranch("Gen_dr_tsTauTau", dr_tsTauTau)
+        self.out.fillBranch("Gen_dr_tsTauZ", dr_tsTauZ)
+        self.out.fillBranch("Gen_dr_tauZ", dr_tauZ)
+        self.out.fillBranch("Gen_dr_zDaus", dr_zDaus)
 
         return True
                     
@@ -117,6 +185,6 @@ class GenProducer(Module):
 genProducerConstr = lambda: GenProducer()
 
 
-files = ["root://cmsxrootd.fnal.gov//store/user/bbarton/TaustarToTauTauZ/SignalMC/taustarToTauTauZ_m3000_2018.root"]
+files = ["root://cmsxrootd.fnal.gov//store/user/bbarton/TaustarToTauTauZ/SignalMC/taustarToTauZ_m3000_2018.root"]
 p = PostProcessor(".", files, cut="1>0", branchsel=None, modules=[genProducerConstr()] )
 p.run()
