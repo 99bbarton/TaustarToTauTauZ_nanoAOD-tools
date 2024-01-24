f87db86938d716230eb5e0c244a33434ff3284af
BenBarton <bbarton@ucdavis.edu>
2024-01-24 10:05:04 -0600

    Script to run nanoAOD modules over multiple files locally

---
+++ runLocal.py
@@ -0,0 +1,22 @@
+from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import PostProcessor
+from PhysicsTools.NanoAODTools.postprocessing.modules.ZProducer import zProducerConstr
+from PhysicsTools.NanoAODTools.postprocessing.modules.GenProducerZTau import genProducerZTauConstr
+from PhysicsTools.NanoAODTools.postprocessing.modules.GenProducerWNu import genProducerWNuConstr
+
+
+
+baseDir = "root://cmsxrootd.fnal.gov//store/user/bbarton/TaustarToTauTauZ/SignalMC/"
+masses = ["250","500","750","1000","1500","2000","2500","3000","3500","4000","4500","5000"]
+decay = "TauZ"
+year = "2018"
+
+for mass in masses:
+    files = [baseDir + decay +"/taustarTo" + decay + "_m" + mass + "_" + year + ".root"]
+    if decay == "TauZ":
+        modules = [genProducerZTauConstr(), zProducerConstr()]
+    elif decay == "WNu":
+        modules = [genProducerWNuConstr()]
+    else: 
+        modules = []
+    p = PostProcessor("data", files, cut="1>0", branchsel=None, postfix="", modules=modules )
+    p.run()
