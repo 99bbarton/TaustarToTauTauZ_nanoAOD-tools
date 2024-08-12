04819740aceab7cc12841ca152f085e671076c3f
Ben Barton <99bbarton@gmail.com>
2024-08-12 09:44:34 +0200

    Basic crab_script.py implementation

--- crab/crab_script.py
+++ crab/crab_script.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 import os
 from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import *
-
 # this takes care of converting the input files from CRAB
 from PhysicsTools.NanoAODTools.postprocessing.framework.crabhelper import inputFiles, runsAndLumis
 
-from PhysicsTools.NanoAODTools.postprocessing.examples.exampleModule import *
-p = PostProcessor(".",
-                  inputFiles(),
-                  "Jet_pt>200",
-                  modules=[exampleModuleConstr()],
-                  provenance=True,
-                  fwkJobReport=True,
-                  jsonInput=runsAndLumis())
+from PhysicsTools.NanoAODTools.postprocessing.modules.GenProducerZTau import genProducerZTauConstr
+from PhysicsTools.NanoAODTools.postprocessing.modules.ZProducer import zProducerConstr
+from PhysicsTools.NanoAODTools.postprocessing.modules.TauProducer import tauProducerConstr
+
+outDir = "./"
+preSelection = "1>0"
+modules = [genProducerZTauConstr(), zProducerConstr(), tauProducerConstr()]
+
+p = PostProcessor(outDir, inputFiles(), preSelection, modules=modules, provenance=True, fwkJobReport=True,)
 p.run()
 
 print("DONE")
