33865622c3b2be0f605758da09b071c38f700394
Ben Barton <99bbarton@gmail.com>
2024-09-04 13:21:35 +0200

    Updated Z producer to choose best candidate and track ee/mumu candidate numbers

--- python/postprocessing/modules/ZProducer.py
+++ python/postprocessing/modules/ZProducer.py
@@ -27,6 +27,8 @@ class ZProducer(Module):
         self.out.branch("Z_pt", "F") #"Pt of ee or mumu pair or jet. 0 default"
         self.out.branch("Z_jetIdxDT", "I") #"Idx to FatJet collection of the most Z-like jet determined using deepTag score. if Z_dm=0"
         self.out.branch("Z_jetIdxPN", "I") #"Idx to FatJet collection of the most Z-like jet determined using particle net score. if Z_dm=0"
+        self.out.branch("Z_nEE", "I") #"Number of candidate ee pairs found"
+        self.out.branch("Z_nMuMu", "I") #"Number of candidate mumu pairs found"
 
     def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
         pass
@@ -40,16 +42,20 @@ class ZProducer(Module):
         Z_dauDR = 0
         Z_jetIdxDT = -1
         Z_jetIdxPN = -1
+        Z_nEE = 0
+        Z_nMuMu = 0
 
         electrons = Collection(event, "Electron")
         for e1Idx, e1 in enumerate(electrons):
             for e2Idx, e2 in enumerate(electrons):
+                if e1Idx == e2Idx:
+                    continue
                 if self.era == 2:
                     cuts = (e1.charge * e2.charge) < 0 #Opposite charge
-                    cuts = cuts and (abs(e1.eta + e1.deltaEtaSC) >= 1.566 or abs( e1.eta + e1.deltaEtaSC) < 1.444) #Fiducial
+                    cuts = cuts and (abs(e1.eta + e1.deltaEtaSC) >= 1.566 or abs( e1.eta + e1.deltaEtaSC) < 1.444)#Fiducial
                     cuts = cuts and (abs(e2.eta + e2.deltaEtaSC) >= 1.566 or abs(e2.eta + e2.deltaEtaSC) < 1.444)
-                    cuts = cuts and (e1.pt >= 20.0 and abs(e1.eta + e1.deltaEtaSC) < 2.5 and e1.mvaFall17V2noIso_WP80) #ID (basic)
-                    cuts = cuts and (e2.pt >= 20.0 and abs(e2.eta + e2.deltaEtaSC) < 2.5 and e2.mvaFall17V2noIso_WP80) #ID (basic)
+                    cuts = cuts and (e1.pt >= 20.0 and abs(e1.eta + e1.deltaEtaSC) < 2.5 and e1.mvaFall17V2noIso_WP80) #ID
+                    cuts = cuts and (e2.pt >= 20.0 and abs(e2.eta + e2.deltaEtaSC) < 2.5 and e2.mvaFall17V2noIso_WP80)
                 elif self.era == 3:
                     cuts = (e1.charge * e2.charge) < 0 #Opposite charge
                     cuts = cuts and (abs(e1.eta + e1.deltaEtaSC) >= 1.566 or abs( e1.eta + e1.deltaEtaSC) < 1.444) #Fiducial
@@ -58,8 +64,9 @@ class ZProducer(Module):
                     cuts = cuts and (e2.pt >= 20.0 and abs(e2.eta + e2.deltaEtaSC) < 2.5 and e2.mvaNoIso_WP80) #ID (basic)
 
                 if cuts:
-                    tempPt = (e1.p4() + e2.p4()).Pt()
-                    if tempPt > Z_pt:
+                    Z_nEE += 1
+                    tempM = (e1.p4() + e2.p4()).M()
+                    if (tempM - 91.18) < (Z_mass - 91.18): #If this pair is closer in mass to the nominal Z
                         Z_mass = (e1.p4()+e2.p4()).M()
                         Z_pt = (e1.p4()+e2.p4()).Pt()
                         if Z_mass >= 60.0 and Z_mass < 130.0: 
@@ -68,21 +75,24 @@ class ZProducer(Module):
                             Z_d2Idx = e2Idx if e1.pt >= e2.pt else e1Idx 
                             Z_dauDR = e1.DeltaR(e2)
         
-        if Z_dm < 0:
-            muons = Collection(event, "Muon")
-            for mu1Idx, mu1 in enumerate(muons):
-                for mu2Idx, mu2 in enumerate(muons):
-                    if (mu1.charge * mu2.charge < 0): #Opposite charge
-                        if (mu1.pt >= 15.0 and abs(mu1.eta) < 2.4 and mu1.mediumId) and (mu2.pt >= 15.0 and abs(mu2.eta) < 2.4 and mu2.mediumId): #ID
-                            tempPt = (mu1.p4() + mu2.p4()).Pt()
-                            if tempPt > Z_pt:
-                                Z_mass = (mu1.p4()+mu2.p4()).M()
-                                Z_pt = (mu1.p4()+mu2.p4()).Pt()
-                                if Z_mass >= 60.0 and Z_mass < 130.0: 
-                                    Z_dm = 2
-                                    Z_d1Idx = mu1Idx if mu1.pt >= mu2.pt else mu2Idx  #daughter 1 is higher pT mu
-                                    Z_d2Idx = mu2Idx if mu1.pt >= mu2.pt else mu1Idx
-                                    Z_dauDR = mu1.DeltaR(mu2)
+        
+        muons = Collection(event, "Muon")
+        for mu1Idx, mu1 in enumerate(muons):
+            for mu2Idx, mu2 in enumerate(muons):
+                if mu2Idx == mu1Idx:
+                    continue
+                if (mu1.charge * mu2.charge < 0): #Opposite charge
+                    if (mu1.pt >= 15.0 and abs(mu1.eta) < 2.4 and mu1.mediumId) and (mu2.pt >= 15.0 and abs(mu2.eta) < 2.4 and mu2.mediumId): #ID
+                        Z_nMuMu += 1
+                        tempM = (mu1.p4() + mu2.p4()).M()
+                        if (tempM - 91.18) < (Z_mass - 91.18):
+                            Z_mass = (mu1.p4()+mu2.p4()).M()
+                            Z_pt = (mu1.p4()+mu2.p4()).Pt()
+                            if Z_mass >= 60.0 and Z_mass < 130.0: 
+                                Z_dm = 2
+                                Z_d1Idx = mu1Idx if mu1.pt >= mu2.pt else mu2Idx  #daughter 1 is higher pT mu
+                                Z_d2Idx = mu2Idx if mu1.pt >= mu2.pt else mu1Idx
+                                Z_dauDR = mu1.DeltaR(mu2)
 
                         
         #TODO Add Z-tautau
@@ -123,6 +133,8 @@ class ZProducer(Module):
         self.out.fillBranch("Z_dauDR", Z_dauDR)
         self.out.fillBranch("Z_jetIdxDT", Z_jetIdxDT)
         self.out.fillBranch("Z_jetIdxPN", Z_jetIdxPN)
+        self.out.fillBranch("Z_nEE", Z_nEE)
+        self.out.fillBranch("Z_nMuMu", Z_nMuMu)
 
         return True
     
